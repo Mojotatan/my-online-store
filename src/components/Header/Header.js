713a8e360d8467dcb8f98604c5d0411b056b34c8
James Byrd <jbyrd@impactnetworking.com>
2021-10-07 11:41:18 -0500

    lesson one: various configs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
       <Container className={styles.headerContainer}>
         <p className={styles.headerTitle}>
           <Link href="/">
-            <a>Hyper Bros. Trading Cards</a>
+            <a>yo check out these trading cards</a>
           </Link>
         </p>
         {/* <p className={styles.headerCart}>
